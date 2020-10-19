d7b8ecea81124b52e4c06d214fd7462f3fbfedb6
Mariusz Krotos <mar.krotos@gmail.com>
2020-10-19 21:10:40 +0200

    Graph implementation

--- algorithms/extra/graph.py
+++ algorithms/extra/graph.py
@@ -1,37 +1,69 @@
 class WeightedGraph:
     """
-    Object oriented implementation of weighted directed graph
+    Object oriented implementation of weighted and directed graph
     """
 
     def __init__(self, initial_node):
-        self.nodes = {}
-        self.nodes[initial_node] = Node(initial_node)
+        self.nodes = {initial_node: Node(initial_node)}
 
     def add(self, new_node, neighbour_of, weight):
         if self.has_node(neighbour_of):
-            self.nodes[new_node] = Node(new_node)
             self.nodes[neighbour_of].add_neighbour(new_node, weight)
+            if not self.has_node(new_node):
+                self.nodes[new_node] = Node(new_node)
         else:
-            raise NeighbourNotPresentException()
+            raise NodeNotPresentException(neighbour_of)
 
     def has_node(self, name: str):
         return self.nodes.__contains__(name)
+
+    def get_neighbours_of(self, node):
+        if self.has_node(node):
+            return self.nodes[node].get_neighbours()
+        raise NodeNotPresentException(node)
+
+    def get_lower_weight_neighbour(self, node):
+        if self.has_node(node):
+            return self.nodes[node].get_lower_weight_neighbour()
+        raise NodeNotPresentException(node)
+
+    def get_weight_between_nodes(self, node, neighbour):
+        if self.has_node(node):
+            return self.nodes[node].get_weight_to_neighbour(neighbour)
+        raise NodeNotPresentException(node)
 
 
 class Node:
     def __init__(self, name):
         self.name = name
-        self.neighbours = []
+        self.neighbours = {}
 
     def add_neighbour(self, node, weight: int):
-        self.neighbours.append(NeighbourNode(node, weight))
+        self.neighbours[node] = weight
+
+    def get_neighbours(self):
+        return [neighbour for neighbour in self.neighbours.keys()]
+
+    def get_lower_weight_neighbour(self):
+        first_neighbour = self.neighbours.popitem()
+        name = first_neighbour[0]
+        lower_weight = first_neighbour[1]
+        for neighbour in self.neighbours.keys():
+            if self.neighbours[neighbour] < lower_weight:
+                name = neighbour
+                lower_weight = self.neighbours[neighbour]
+        return name
+
+    def get_weight_to_neighbour(self, neighbour):
+        if self.has_neighbour(neighbour):
+            return self.neighbours[neighbour]
+        return None
+
+    def has_neighbour(self, neighbour):
+        return self.neighbours.__contains__(neighbour)
 
 
-class NeighbourNode:
-    def __init__(self, node: Node, weight: int):
-        self.node = node
-        self.weight = weight
-
-
-class NeighbourNotPresentException(Exception):
-    pass
+class NodeNotPresentException(Exception):
+    def __init__(self, node_name):
+        self.message = f'Node [{node_name}] do not exists'
+        super().__init__(self.message)
--- test/test_graph.py
+++ test/test_graph.py
@@ -1,6 +1,13 @@
 from unittest import TestCase
 
-from algorithms.extra.graph import WeightedGraph
+from algorithms.extra.graph import *
+
+FOURTH_NODE = 'fourth'
+
+FIRST_NODE = 'first'
+SECOND_NODE = 'second'
+THIRD_NODE = 'third'
+NOT_EXISTING_NODE = 'not existing node'
 
 
 class Test(TestCase):
@@ -8,16 +15,90 @@ class Test(TestCase):
     def test_initializing(self):
         # given
         # when
-        graph = WeightedGraph('first')
+        graph = WeightedGraph(FIRST_NODE)
 
         # then
-        self.assertTrue(graph.has_node('first'))
+        self.assertTrue(graph.has_node(FIRST_NODE))
 
     def test_add_new_node(self):
         # given
-        graph = WeightedGraph('first')
+        graph = WeightedGraph(FIRST_NODE)
         # when
-        graph.add('second', 'first', 2)
+        graph.add(SECOND_NODE, FIRST_NODE, 2)
         # then
-        self.assertTrue(graph.has_node('second'))
-        self.assertEqual(['second'], graph.getNeighboursOf('first'))
+        self.assertTrue(graph.has_node(SECOND_NODE))
+        self.assertEqual([SECOND_NODE], graph.get_neighbours_of(FIRST_NODE))
+
+    def test_raise_error_when_neighbour_not_present_on_adding_new_node(self):
+        # given
+        graph = WeightedGraph(FIRST_NODE)
+        # then
+        with self.assertRaises(NodeNotPresentException):
+            # when
+            graph.add(SECOND_NODE, NOT_EXISTING_NODE, 2)
+
+    def test_raise_error_when_node_not_present_on_getting_neighbours(self):
+        # given
+        graph = WeightedGraph(FIRST_NODE)
+        # then
+        with self.assertRaises(NodeNotPresentException):
+            # when
+            graph.get_neighbours_of(NOT_EXISTING_NODE)
+
+    def test_add_new_link_to_existing_nodes(self):
+        # given
+        graph = WeightedGraph(FIRST_NODE)
+        graph.add(SECOND_NODE, FIRST_NODE, 2)
+        graph.add(THIRD_NODE, FIRST_NODE, 3)
+        graph.add(FOURTH_NODE, SECOND_NODE, 3)
+        # when
+        graph.add(SECOND_NODE, THIRD_NODE, 1)
+        # then
+        self.assertEqual([SECOND_NODE, THIRD_NODE], graph.get_neighbours_of(FIRST_NODE))
+        self.assertEqual([FOURTH_NODE], graph.get_neighbours_of(SECOND_NODE))
+        self.assertEqual([SECOND_NODE], graph.get_neighbours_of(THIRD_NODE))
+
+    def test_get_lower_weight_neighbour(self):
+        # given
+        graph = WeightedGraph(FIRST_NODE)
+        graph.add(SECOND_NODE, FIRST_NODE, 2)
+        graph.add(THIRD_NODE, FIRST_NODE, 3)
+        graph.add(FOURTH_NODE, FIRST_NODE, 1)
+        # when
+        lower_cost_node = graph.get_lower_weight_neighbour(FIRST_NODE)
+        # then
+        self.assertEqual(FOURTH_NODE, lower_cost_node)
+
+    def test_raise_error_when_node_not_present_on_get_lower_weight_neighbour(self):
+        # given
+        graph = WeightedGraph(FIRST_NODE)
+        # then
+        with self.assertRaises(NodeNotPresentException):
+            # when
+            graph.get_lower_weight_neighbour(NOT_EXISTING_NODE)
+
+    def test_get_weight_between_nodes(self):
+        # given
+        graph = WeightedGraph(FIRST_NODE)
+        graph.add(SECOND_NODE, FIRST_NODE, 2)
+        # when
+        weight = graph.get_weight_between_nodes(FIRST_NODE, SECOND_NODE)
+        # then
+        self.assertEqual(2, weight)
+
+    def test_raise_exception_when_first_node_not_present_on_getting_weight(self):
+        # given
+        graph = WeightedGraph(FIRST_NODE)
+        graph.add(SECOND_NODE, FIRST_NODE, 2)
+        # then
+        with self.assertRaises(NodeNotPresentException):
+            # when
+            graph.get_weight_between_nodes(NOT_EXISTING_NODE, SECOND_NODE)
+
+    def test_return_none_as_weight_on_not_existing_second_node(self):
+        # given
+        graph = WeightedGraph(FIRST_NODE)
+        # when
+        weight = graph.get_weight_between_nodes(FIRST_NODE, SECOND_NODE)
+        # then
+        self.assertIsNone(weight)
